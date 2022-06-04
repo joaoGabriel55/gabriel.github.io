0b660cdfc3b0cb3afd39064ea19206440415421a
joaoGabriel55 <j.quaresmasantos_98@hotmail.com>
2022-06-04 16:59:39 -0300

    Added Pull requests section

--- js/index.js
+++ js/index.js
@@ -1,4 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
   const parallax = document.querySelectorAll(".parallax");
+  const tabs = document.querySelectorAll(".tabs");
+
   M.Parallax.init(parallax);
+  M.Tabs.init(tabs);
 });
