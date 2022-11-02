4920b21d857a72b307c88ca09fe767a9984d751a
joaoGabriel55 <j.quaresmasantos_98@hotmail.com>
2022-11-02 11:09:09 -0300

    fix: css style

--- js/index.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const parallax = document.querySelectorAll(".parallax");
-  const tabs = document.querySelectorAll(".tabs");
-
-  M.Parallax.init(parallax);
-  M.Tabs.init(tabs);
-});
